8e63f9470114cddf644731f3aa6bb133b361abdb
linyuwei <linyuwei.designer@gmail.com>
2020-12-11 00:40:46 +0800

    Initial version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { HomePage } from './homepage.component';
+
 import './App.css';
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    )
   }
 }
 
