f021d982197d87f6125026179d6382ca953a9991
linyuwei <linyuwei.designer@gmail.com>
2020-12-11 19:56:10 +0800

    Updating app with a shop page, collection preview component and collection item component.

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
-import { HomePage } from './pages/homepage/homepage.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 import './App.css';
 
-const TopicsList = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <h1>TOPIC LIST PAGE</h1>
-      <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
-      <Link to={`${props.match.url}/14`}>TO TOPIC 14</Link>
-      <Link to={`${props.match.url}/15`}>TO TOPIC 15</Link>
-    </div>
-  )
-}
+// const TopicsList = (props) => {
+//   console.log(props)
+//   return (
+//     <div>
+//       <h1>TOPIC LIST PAGE</h1>
+//       <Link to={`${props.match.url}/13`}>TO TOPIC 13</Link>
+//       <Link to={`${props.match.url}/14`}>TO TOPIC 14</Link>
+//       <Link to={`${props.match.url}/15`}>TO TOPIC 15</Link>
+//     </div>
+//   )
+// }
 
-const TopicDetail = (props) => {
-  console.log(props)
-  return (
-    <div>
-      <Link to='/topics'>Topics</Link>
-      <button onClick={() => { props.history.push('/topics')}}>Topics</button>
-      <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
-    </div>
-  )
-}
+// const TopicDetail = (props) => {
+//   console.log(props)
+//   return (
+//     <div>
+//       <Link to='/topics'>Topics</Link>
+//       <button onClick={() => { props.history.push('/topics')}}>Topics</button>
+//       <h1>TOPIC DETAIL PAGE: {props.match.params.topicId}</h1>
+//     </div>
+//   )
+// }
 
 function App() {
   return (
     <div>
       <switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/topics' component={TopicsList} />        
+        <Route path='/shop' component={ShopPage} />
+        {/* <Route exact path='/topics' component={TopicsList} />
         <Route path='/topics/:topicId' component={TopicDetail} />
         <Route exact path='/blog/topics' component={TopicsList} />
-        <Route path='/blog/topics/:topicId' component={TopicDetail} />
+        <Route path='/blog/topics/:topicId' component={TopicDetail} /> */}
 
       </switch>
     </div>
